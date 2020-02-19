e48eb729ffafcb5deff84ef8154404fd41ee4f88
MAJD AR <zzmmzzmm32192@gmail.com>
2020-02-19 23:49:52 +0200

    basic score system + coins sprites from assest store

---
+++ scenes/sccore.gd
@@ -0,0 +1,19 @@
+extends Label
+
+#sccore:
+var sccore : int = 0
+
+#nodes:
+onready var spawn : KinematicBody2D = get_parent().get_parent().get_parent().get_node("game_spawner")
+
+func _on_xp_timeout():
+	
+	if spawn.dif == 0 or spawn.dif == 1:
+		sccore += 5
+	
+	elif spawn.dif >= 2:
+		sccore += 10 
+
+func _physics_process(delta):
+	
+	text = "sccore : " + str(sccore)
